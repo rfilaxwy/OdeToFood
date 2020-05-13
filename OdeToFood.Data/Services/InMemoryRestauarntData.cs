860aeef90f2526479595830b51532e5ca2c9083c
Ryan Filax-Wylie <ryanfilaxwylie@gmail.com>
2020-05-13 13:52:22 -0600

    Services for restaurant data

---
+++ OdeToFood.Data/Services/InMemoryRestauarntData.cs
@@ -0,0 +1,25 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestauarntData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestauarntData()
+        {
+            restaurants = new List<Restaurant>();
+            {
+                new Restaurant { Id = 1, Name = "Veto'sandwichs", Cuisine = CuisineType.Italian };
+                new Restaurant { Id = 2, Name = "Monde", Cuisine = CuisineType.French };
+                new Restaurant { Id = 3, Name = "Fuko", Cuisine = CuisineType.Japanese }
+            }
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
